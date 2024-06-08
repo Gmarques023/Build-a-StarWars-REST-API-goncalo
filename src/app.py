fdbcece3e3cbadff6cec87d678de847f0698be3f
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-06-08 17:52:52 +0000

    update db people and planets

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,25 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = People.query.all()
+    all_people = list(map(lambda x: x.serialize(), people))
+    
+    return jsonify(all_people), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if person is None:
+        return jsonify({"error": "Person not found"}), 404
+    return jsonify(person.serialize()), 200
 
-    return jsonify(response_body), 200
+@app.route('/planets/', methods=['GET'])
+def get_planet():
+    planet = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planet))
+    return jsonify(all_planets), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class User(db.Model):
+    
+class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    name = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(250))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
         }
+    
+class Planets(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(250),nullable=False)
+    population=db.Column(db.Integer)
+    terrain=db.Column(db.String(250))
+    climate=db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "population":self.population,
+            "terrain": self.terrain,
+            "climate":self.climate
+        }
+
+
